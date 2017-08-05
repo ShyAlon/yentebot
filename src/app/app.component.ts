bf04fb77850c166a4c331a26b9803d3be017e30a
Shy Alon <shy.alon@gmail.com>
2017-08-05 22:44:44 +0300

    adding firebase - not ours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyALKfevapBOYK202f6k5mPPfMrT1MHDv5A",
+      authDomain: "bill-tracker-e5746.firebaseapp.com",
+      databaseURL: "https://bill-tracker-e5746.firebaseio.com",
+      storageBucket: "bill-tracker-e5746.appspot.com",
+      messagingSenderId: "508248799540"
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
